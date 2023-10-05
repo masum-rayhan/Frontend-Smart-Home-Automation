ce26a35eeaceacee8db1081bcb22addc3619d061
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-10-05 07:01:44 +0600

    feat: add delete button for deleting each device and adjust the padding of the device box

--- src/components/DeviceBox.jsx
+++ src/components/DeviceBox.jsx
@@ -5,6 +5,7 @@ import {
   Typography,
   useTheme,
   useMediaQuery,
+  Grid,
 } from "@mui/material";
 import React, { useState } from "react";
 import { tokens } from "../theme";
@@ -12,58 +13,90 @@ import ToggleOffIcon from "@mui/icons-material/ToggleOff";
 import ToggleOnIcon from "@mui/icons-material/ToggleOn";
 import EmojiObjectsOutlinedIcon from "@mui/icons-material/EmojiObjectsOutlined";
 import EmojiObjectsIcon from "@mui/icons-material/EmojiObjects";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const DeviceBox = ({ device, index }) => {
   const theme = useTheme();
-  const smScreen = useMediaQuery(theme.breakpoints.up("sm"));
   const colors = tokens(theme.palette.mode);
 
   // Initialize state for the device's on/off status
   const [isDeviceOn, setIsDeviceOn] = useState(false);
   const [deviceType, setDeviceType] = useState("outline");
-   // Toggle the device's status when the button is clicked
-   const handleToggle = () => {
+
+  const [isHovered, setIsHovered] = useState(false);
+
+  // Toggle the device's status when the button is clicked
+  const handleToggle = () => {
     setIsDeviceOn((prevIsDeviceOn) => !prevIsDeviceOn);
-    setDeviceType((prevDeviceType) => 
+    setDeviceType((prevDeviceType) =>
       prevDeviceType === "outline" ? "filled" : "outline"
     );
   };
-  // console.log(device);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
 
   return (
     <>
-      <Box
+      <Grid
         key={`${device}-${index}`}
-        display="flex"
+        container
         justifyContent="space-between"
         alignItems="center"
         borderBottom={`4px solid ${colors.primary[500]}`}
-        p="15px"
+        p="3px"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        style={{ position: "relative" }}
       >
-        <Box color={colors.greenAccent[500]} p="5px 10px" borderRadius="4px">
-          {deviceType === "outline" ? (
-            <EmojiObjectsOutlinedIcon style={{ fontSize: "45px" }} />
-          ) : (
-            <EmojiObjectsIcon style={{ fontSize: "45px" }} />
+        {/* <Grid item xs={2}> */}
+          {isHovered && (
+            <IconButton
+              color="error"
+              // onClick={() => onDelete(device.id)}
+              style={{ visibility: isHovered ? "visible" : "hidden" }}
+            >
+              <DeleteIcon />
+            </IconButton>
           )}
-        </Box>
-        <Box>
-          {/* Render device information here */}
-          <Typography color={colors.grey[100]}>{device.name}</Typography>
-        </Box>
-        <Box
-          color={colors.greenAccent[500]}
-          p="5px 10px"
-          borderRadius="4px"
-          onClick={ handleToggle }
-        >
-          {isDeviceOn ? (
-            <ToggleOnIcon color="primary" style={{ fontSize: "45px" }} />
-          ) : (
-            <ToggleOffIcon color="disabled" style={{ fontSize: "45px" }} />
-          )}
-        </Box>
-      </Box>
+        {/* </Grid> */}
+        <Grid item xs={2} sm={1} style={{ marginLeft: isHovered ? "10px" : "0" }}>
+          <Box color={colors.greenAccent[500]} p="5px 10px" borderRadius="4px">
+            {deviceType === "outline" ? (
+              <EmojiObjectsOutlinedIcon style={{ fontSize: "45px" }} />
+            ) : (
+              <EmojiObjectsIcon style={{ fontSize: "45px" }} />
+            )}
+          </Box>
+        </Grid>
+
+        <Grid item xs={4} style={{ textAlign: "center" }}>
+          {/* <Box> */}
+            {/* Render device information here */}
+            <Typography color={colors.grey[100]}>{device.name}</Typography>
+          {/* </Box> */}
+        </Grid>
+
+        <Grid item>
+          <Box
+            color={colors.greenAccent[500]}
+            p="5px 10px"
+            borderRadius="4px"
+            onClick={handleToggle}
+          >
+            {isDeviceOn ? (
+              <ToggleOnIcon color="primary" style={{ fontSize: "45px" }} />
+            ) : (
+              <ToggleOffIcon color="disabled" style={{ fontSize: "45px" }} />
+            )}
+          </Box>
+        </Grid>
+      </Grid>
     </>
   );
 };
