ecb4b218f0dd36f53085f3bb9def503b72cbd79e
masum-rayhan <masumrayhan76@gmail.com>
2023-10-08 03:51:58 +0600

    comment out Invoices as it is no longer in need

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/team" element={<Team />} />
                 <Route path="/contacts" element={<Contacts />} />
-                <Route path="/invoices" element={<Invoices />} />
+                {/* <Route path="/invoices" element={<Invoices />} /> */}
               </Routes>
             </main>
           </div>
